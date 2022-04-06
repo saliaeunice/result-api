05e72117caca0598d6c0a40dc756d546d2fc5e4f
Salia Eunice <101419339+saliaeunice@users.noreply.github.com>
2022-04-06 17:12:23 +0100

    my first backend

---
+++ results.js
@@ -0,0 +1,9 @@
+const results=[
+{name:"Windy Dari", class:"Form One", marks:8},
+{name:"Zakaria Ab", class:"Form One", marks:6},
+{name:"Flex Dass", class:"Form One", marks:2},
+{name:"Awudu Dery", class:"Form One", marks:10},
+{name:"Dady Ama", class:"Form One", marks:7}
+]
+
+module.exports=results
